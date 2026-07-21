baa9886bf78ad443f12527bdc6ae35b1b4dfd93b
AmirHossein <fayamir54@gmail.com>
2026-07-21 20:52:43 +0330

    init user-entity

---
+++ backend/src/modules/auth/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { BaseApplicationEntity } from '@shared/abstract';
+import { ApplicationRoles, GenderEnum } from '@shared/enums';
+import { Column, Entity, Index } from 'typeorm';
+
+@Entity()
+export class User extends BaseApplicationEntity {
+  @Column({ length: 100, nullable: true })
+  @Index()
+  fullName?: string;
+
+  @Column({ length: 11, unique: true, nullable: true })
+  phone?: string;
+
+  @Column({ nullable: true })
+  password?: string;
+
+  @Column({
+    type: 'enum',
+    enum: ApplicationRoles,
+    default: ApplicationRoles.USER,
+  })
+  role: ApplicationRoles;
+
+  @Column({ unique: true, nullable: true })
+  email?: string;
+
+  @Column({
+    type: 'enum',
+    enum: GenderEnum,
+    nullable: true,
+  })
+  gender?: GenderEnum;
+
+  @Column({ type: 'date', nullable: true })
+  birthDate?: Date;
+
+  @Column({ nullable: true })
+  refreshToken?: string;
+}
