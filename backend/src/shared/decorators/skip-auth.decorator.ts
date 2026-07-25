1a2e8886b8dccf48f10cd05ef0101fbe9229ce1f
ZahraGolabi <zgholabi@gmail.com>
2026-07-25 17:51:49 +0330

    rename skipAuth to PublicEndPoint

--- backend/src/shared/decorators/skip-auth.decorator.ts
+++ backend/src/shared/decorators/skip-auth.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 import { SKIP_AUTH } from '@shared/constants/tokens';
 
-export const SkipAuth = () => {
+export const PublicEndPoint = () => {
   return SetMetadata(SKIP_AUTH, true);
 };
