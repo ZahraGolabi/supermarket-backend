c7712d7045ac22b0f5b1fa2fa1054dc487b6248b
AmirHossein <fayamir54@gmail.com>
2026-07-25 18:17:21 +0330

    add owner-seeder

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,7 +1,9 @@
-import { Global, Module } from '@nestjs/common';
+import dataSource from '@config/data-source';
+import { Global, Module, OnApplicationBootstrap } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CacheService } from '@shared/providers';
+import { OwnerSeeder } from 'src/seeders/owner.seeder';
 import { AuthController } from './controllers/auth.controller';
 import { User } from './entities/user.entity';
 import { AuthService } from './services/auth.service';
@@ -15,4 +17,8 @@ import { OtpService } from './services/otp.service';
   providers: [AuthService, JwtService, JwtAppService, CacheService, OtpService],
   exports: [JwtAppService, CacheService],
 })
-export class AuthModule {}
+export class AuthModule implements OnApplicationBootstrap {
+  async onApplicationBootstrap() {
+    OwnerSeeder(dataSource);
+  }
+}
