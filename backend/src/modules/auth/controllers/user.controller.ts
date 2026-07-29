fa0ff78dfbd5a7ab96220b928aaac2adbcc5524f
AmirHossein <fayamir54@gmail.com>
2026-07-29 18:55:00 +0330

    add update-profile end-point

--- backend/src/modules/auth/controllers/user.controller.ts
+++ backend/src/modules/auth/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Put } from '@nestjs/common';
 import { type JwtPayload } from '@shared/@types';
 import { AuthorizeByRole, User } from '@shared/decorators';
 import { ApplicationRoles } from '@shared/enums';
+import { UpdateProfileDto } from '../dto/update-profile.dto';
 import { UserService } from '../services/user.service';
 
 @Controller('users')
@@ -12,5 +13,11 @@ export class UserController {
   @AuthorizeByRole([ApplicationRoles.ALL])
   async getMe(@User() user: JwtPayload) {
     return this.service.me(user.sub);
+  }
+
+  @Put('update-profile')
+  @AuthorizeByRole([ApplicationRoles.ALL])
+  async updateProfile(@User() user: JwtPayload, @Body() dto: UpdateProfileDto) {
+    return await this.service.updateProfile(user.sub, dto);
   }
 }
--- backend/src/modules/auth/services/user.service.ts
+++ backend/src/modules/auth/services/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateProfileDto } from '../dto/update-profile.dto';
 import { User } from '../entities/user.entity';
 
 @Injectable()
@@ -11,5 +12,19 @@ export class UserService {
 
   async me(id: string) {
     return this.repo.findOne({ where: { id } });
+  }
+
+  async findOne(id: string) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException();
+
+    return user;
+  }
+
+  async updateProfile(id: string, dto: UpdateProfileDto) {
+    const user = await this.findOne(id);
+
+    Object.assign(user, dto);
+    return await this.repo.save(user);
   }
 }
